d79ddca2ab0186646144330fccd0dd725d03b58f
cy <sd1632455123@gmail.com>
2019-12-30 17:27:10 +0800

    new

--- src/views/WeatherStation/WeatherStation.vue
+++ src/views/WeatherStation/WeatherStation.vue
@@ -5,15 +5,10 @@
         <el-breadcrumb-item><i class="el-icon-tickets"></i> 气象站</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
-    <!-- <el-row :gutter="20">
-      <el-col :span="12">
-        <x-outside :info="outsideInfo"></x-outside>
-      </el-col>
-    </el-row> -->
     <div v-if="weatherStationFlag" class="content">
       <el-row :gutter="20" v-for="(x, i) in weatherStationState" :key="i" class="row">
         <el-col :span="24">
-          <x-weather-station></x-weather-station>
+          <x-weather-station :info="x"></x-weather-station>
         </el-col>
       </el-row>
     </div>
@@ -21,12 +16,11 @@
 </template>
 
 <script>
-import XOutside from './Outside.vue'
 import XWeatherStation from './WeatherStationComponent.vue'
 
 export default {
   components: {
-    XOutside, XWeatherStation
+    XWeatherStation
   },
   data() {
     return {
@@ -34,38 +28,6 @@ export default {
       weatherStationFlag: false,
       // 用户所有的气象站参数(实时数据)
       weatherStationState: [],
-      // 棚外参数
-      outsideInfo: [{
-        name: '降雨量',
-        key: 'rainfall',
-        value: ''
-      },{
-        name: '光照度',
-        key: 'illuminance',
-        value: ''
-      },{
-        name: '温度',
-        key: 'temperature',
-        value: ''
-      },{
-        name: '风速',
-        key: 'windspeed',
-        value: ''
-      },{
-        name: '湿度',
-        key: 'humidity',
-        value: ''
-      },{
-        name: '气压',
-        key: 'pressure',
-        value: ''
-      },{
-        name: '风向',
-        key: 'winddirection',
-        value: ''
-      }],
-      // 棚内参数
-      insideInfo: [],
       // 定时器的名称
       update: null
     }
@@ -79,31 +41,13 @@ export default {
           token: localStorage.getItem('user_token')
         }
       }).then(res => {
-        console.log(res);
-        for (let i in res.data.data) {
-          this.weatherStationState[i] = res.data.data[i]
-        }
-        this.weatherStationFlag = true;
-      }).catch(err => {
-        console.log(err)
-      })
-    },
-    // 获得棚外参数
-    getState(id) {
-      this.axios({
-        url: 'http://60.190.23.22:8889/fertilizer_distributor/api/do.jhtml?router=appApiService.getRealTimeData',
-        params: {
-          fd_id: id
-        }
-      }).then(res => {
         // console.log(res);
-        for (let i in this.outsideInfo) {
-          if (this.outsideInfo[i].key == 'humidity') {
-            this.outsideInfo[i].value = res.data.data[this.outsideInfo[i].key].substr(0, res.data.data[this.outsideInfo[i].key].length - 1);
-          } else {
-            this.outsideInfo[i].value = res.data.data[this.outsideInfo[i].key];
+        for (let i in res.data.data) {
+          if (res.data.data[i].species == 'weather_station') {
+            this.weatherStationState[i] = res.data.data[i]
           }
         }
+        this.weatherStationFlag = true;
       }).catch(err => {
         console.log(err)
       })
--- src/views/WeatherStation/WeatherStationComponent.vue
+++ src/views/WeatherStation/WeatherStationComponent.vue
@@ -1,17 +1,206 @@
 <template>
   <el-card shadow="hover">
     <div slot="header">
-      <span>气象站数据</span>
+      <span>{{this.info.jiqiname}}</span>
     </div>
+    <el-row :gutter="20">
+      <el-col :span="10">
+        <el-table :data="weatherStationState" :header-cell-style="{background:'#f5f7fa'}" border class="weather-station-table">
+          <el-table-column label="参数" align="center">
+            <template slot-scope="scope">
+              <el-button
+                type="text"
+                @click="stateClick(scope)"
+                v-if="scope.row.key != 'winddirection' || scope.row.value === ''">
+                {{scope.row.name}}
+              </el-button>
+              <span v-else>{{scope.row.name}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column prop="value" label="数值" align="center"></el-table-column>
+        </el-table>
+      </el-col>
+      <el-col :span="14">
+        <el-date-picker
+        v-model="activeDate"
+        value-format="yyyy-MM-dd"
+        type="date"
+        placeholder="选择日期"
+        size="small"
+        class="date-picker"
+        @change="dateClick"></el-date-picker>
+        <div class="chart" :id="info.fd_id+info.species"></div>
+      </el-col>
+    </el-row>
   </el-card>
 </template>
 
 <script>
-export default {
+// vue文件中引入echarts工具
+const echarts = require("echarts/lib/echarts");
+require("echarts/lib/chart/line");
+// 以下的组件按需引入
+require("echarts/lib/component/tooltip"); // tooltip组件
 
+export default {
+  props: {
+    info: {
+      type: Object
+    }
+  },
+  data() {
+    return {
+      // 激活的日期
+      activeDate: "",
+      // 激活的参数
+      activeKey: "temperature",
+      // 折线图的基础设置
+      option: {
+        tooltip: {
+          trigger: "axis"
+        },
+        grid: {
+          y: "15%"
+        },
+        xAxis: {
+          name: "时间",
+          type: "category",
+          boundaryGap: false,
+          data: []
+        },
+        yAxis: {
+          name: "温度",
+          type: "value",
+          // y轴单位
+          // axisLabel:{formatter:'{value} %'}
+        },
+        series: [
+          {
+            data: [],
+            type: "line"
+          }
+        ]
+      },
+      // 定时器的名称
+      update: null
+    }
+  },
+  computed: {
+    // 气象站参数
+    weatherStationState() {
+      return [{
+        name: '降雨量',
+        key: 'rainfall',
+        value: this.info.rainfall
+      },{
+        name: '光照度',
+        key: 'illuminance',
+        value: this.info.illuminance
+      },{
+        name: '温度',
+        key: 'temperature',
+        value: this.info.temperature
+      },{
+        name: '风速',
+        key: 'windspeed',
+        value: this.info.windspeed
+      },{
+        name: '湿度',
+        key: 'humidity',
+        value: this.info.humidity
+      },{
+        name: '气压',
+        key: 'pressure',
+        value: this.info.pressure
+      },{
+        name: '风向',
+        key: 'winddirection',
+        value: this.info.winddirection
+      }]
+    }
+  },
+  methods: {
+    // 获得折线图的数据
+    getChartData(id, key, date) {
+      // 先清空原有数据
+      this.option.xAxis.data = [];
+      this.option.series[0].data = [];
+      this.axios({
+        url:
+          "http://60.190.23.22:8889/fertilizer_distributor/api/do.jhtml?router=appApiService.getDatasnqx",
+        params: {
+          token: localStorage.getItem('user_token'),
+          fd_id: id,
+          date: date
+        }
+      }).then(res => {
+        // console.log(res);
+        for (let i in res.data.data) {
+          this.option.xAxis.data[i] = res.data.data[i].date.substr(11, 8);
+          if (key == 'humidity') {
+            this.option.series[0].data[i] = res.data.data[i][key].substr(0,res.data.data[i][key].length-1);
+          } else {
+            this.option.series[0].data[i] = res.data.data[i][key];
+          }
+        }
+        this.draw();
+      }).catch(err => {
+        console.log(err);
+      });
+    },
+    // 绘制折线图
+    draw() {
+      const myChart = echarts.init(document.getElementById(this.info.fd_id + this.info.species));
+      myChart.setOption(this.option);
+    },
+    // 获得当前时间
+    getNowDate() {
+      let date = new Date();
+      let seperator1 = "-";
+      let year = date.getFullYear();
+      let month = date.getMonth() + 1;
+      let strDate = date.getDate();
+      if (month >= 1 && month <= 9) {
+        month = "0" + month;
+      }
+      if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate;
+      }
+      let currentdate = year + seperator1 + month + seperator1 + strDate;
+      return currentdate;
+    },
+    // 参数点击事件
+    stateClick(scope) {
+      this.activeKey = scope.row.key;
+      this.option.yAxis.name = scope.row.name;
+      // console.log(this.activeKey);
+      this.getChartData(this.info.fd_id, this.activeKey, this.activeDate);
+    },
+    // 日期点击事件
+    dateClick() {
+      // console.log(this.activeDate);
+      this.getChartData(this.info.fd_id, this.activeKey, this.activeDate);
+    },
+  },
+  mounted() {
+    this.activeDate = this.getNowDate();
+    this.getChartData(this.info.fd_id, this.activeKey, this.activeDate);
+  },
+  activated() {
+    // 每5秒更新一次数据
+    this.update = setInterval(() => {
+      this.getChartData(this.$store.state.activeId, this.activeKey, this.activeDate);
+    }, 5000);
+  },
+  deactivated() {
+    clearInterval(this.update);
+  }
 }
 </script>
 
 <style scoped>
-
+  /* 必须在组件内部就给高度，因为先渲染组件 */
+  .chart {
+    height: 450px;
+  }
 </style>
