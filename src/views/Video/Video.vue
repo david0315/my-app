e6550cbfce78d1f2d1536ec6d3a967396e7d3151
cy <sd1632455123@gmail.com>
2020-01-10 16:16:32 +0800

    new

--- src/views/Video/Video.vue
+++ src/views/Video/Video.vue
@@ -5,27 +5,20 @@
         <el-breadcrumb-item><i class="el-icon-video-camera"></i> 现场监控</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
-    <div class="container clear">
+    <div class="container" v-if="$store.state.videoFlag">
       <el-row :gutter="20">
-        <el-col :span="6">  
+        <el-col :span="4">
           <el-menu
-          :default-active="activeIndex">
-            <el-menu-item index="1">
-              <span slot="title">导航一</span>
-            </el-menu-item>
-            <el-menu-item index="2">
-              <span slot="title">导航二</span>
-            </el-menu-item>
-            <el-menu-item index="3">
-              <span slot="title">导航三</span>
-            </el-menu-item>
-            <el-menu-item index="4">
-              <span slot="title">导航四</span>
+          default-active="0"
+          @select="changeVideo"
+          class="videoList">
+            <el-menu-item :index="String(i)" v-for="(x,i) in $store.state.userInfo.cameraList" :key="i">
+              <span slot="title">{{x.deviceSerial}}</span>
             </el-menu-item>
           </el-menu>
         </el-col>
-        <el-col :span="18" class="video-content">
-          <iframe :src="src" width="100%" :height="height" allowfullscreen v-if="$store.state.videoFlag"></iframe>
+        <el-col :span="20" class="video-content">
+          <iframe :src="src" width="100%" height="720" allowfullscreen id="ysopen"></iframe>
         </el-col>
       </el-row>
     </div>
@@ -41,20 +34,28 @@ export default {
   },
   data() {
     return {
-      activeIndex: '1'
+      activeIndex: 0,
     }
   },
   computed: {
     src() {
-      return "https://open.ys7.com/ezopen/h5/iframe?url=ezopen://open.ys7.com/" + this.$store.state.userInfo.cameraList[0].deviceSerial + "/" + this.$store.state.userInfo.cameraList[0].channelNo + ".live&autoplay=1&accessToken=" + this.$store.state.userInfo.accessToken
-    },
-    height() {
-      return document.querySelector('.video-content').offsetWidth*3/5
+      return "https://open.ys7.com/ezopen/h5/iframe?url=ezopen://open.ys7.com/" + this.$store.state.userInfo.cameraList[Number(this.activeIndex)].deviceSerial + "/" + this.$store.state.userInfo.cameraList[Number(this.activeIndex)].channelNo + ".live&autoplay=1&accessToken=" + this.$store.state.userInfo.accessToken
     }
   },
+  methods: {
+    changeVideo(index) {
+      this.activeIndex = index;
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+  .videoList {
+    overflow-y: scroll;
+    height: 720px;
+  }
+  .videoList::-webkit-scrollbar {
+      width: 0;
+  }
 </style>
